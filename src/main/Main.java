cd6c8c0812c60b4a112b20135081e686e108e15a
raina <skgus5598@gmail.com>
2024-03-26 21:30:06 +0000

    within 2-3hours results

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,18 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("-----------------------------------");
         int[] mars = {5,3};
         String[] coordinate = { "3", "2", "N"};
-        String[] positions = { "L","L","F","F","F","L","F","L","F","L"};
         String[] positions2 = {"F","R","R","F","L","L","F","F","R","R","F","L","L"};
+        //String[] positions = { "L","L","F","F","F","L","F","L","F","L"};
+
+
+
         String  result = new RobotChallenge().findPositions(mars, coordinate, positions2);
         System.out.println("-----------------------------------");
 
--- src/main/RobotChallenge.java
+++ src/main/RobotChallenge.java
@@ -3,58 +3,46 @@ package main;
 public class RobotChallenge {
     public String findPositions(int[] mars, String[] coordinates, String[] positions){
         String result = "";
-        /*
-            5 3
-            0 3 W
-            LLFFFLFLFL
-         */
-        //Dead Line : x=5, y=3, x=4&y=0 or y=3
-        int[] scent = {0,0}; // dead coordinates
-        String scentN = "";
 
-        int [] location = {Integer.parseInt(coordinates[0]), Integer.parseInt(coordinates[1])}; // horse
-        //location { 1, 1 }
+        //Scent zone
+        int[] scent = {0,0}; // initialize
+        String scentN = ""; // "" or "LOST"
+
+        //Initialize robot's location
+        int [] location = {Integer.parseInt(coordinates[0]), Integer.parseInt(coordinates[1])};
 
 
-        String facing = coordinates[2]; // W
+        String facing = coordinates[2]; //facing side
         String[] sides = { "E", "S", "W", "N"};
         int facingIdx = 0;
 
-        for(int i=0; i < sides.length; i++){
+        for(int i=0; i < sides.length; i++){ // set up by idx
             if(facing.equals(sides[i])){
-                facingIdx = i; // 2
-                System.out.println("facingidx : " + i +", " + facing);
+                facingIdx = i;
                 break;
             }
         }
 
         //LL FFF LFLFL
         for(int i=0; i < positions.length; i++) {
-            System.out.println("??" + positions[i] );
             if(positions[i].equals("R")){
                 facingIdx ++;
                 if(facingIdx == 4) facingIdx = 0;
                 facing = sides[facingIdx];
-                System.out.println("turn right : " + facing);
             }else if(positions[i].equals("L")){
                 facingIdx --;
                 if(facingIdx == -1) facingIdx = 3;
                 facing = sides[facingIdx];
-                System.out.println("turn left : " + facing + " , facing idx : " + facingIdx);
             }else if(positions[i].equals("F")){
                 //moving forward
                     if(facing.equals("E")){ // x+1
                         location[0] ++;
-                        System.out.println("E) X+1 :" + location[0]);
                     }else if(facing.equals("S")){ //y-1
                         location[1] --;
-                        System.out.println("S) Y-1 :" + location[1]);
                     }else if(facing.equals("W")){ //x-1
                         location[0] --;
-                        System.out.println("W) X-1 :" + location[0]);
-                    }else{ //N y+1
+                    }else{ //N :: y+1
                         location[1] ++;
-                        System.out.println("N) Y+1 :" + location[1]);
                     }
             }
         }
@@ -64,7 +52,6 @@ public class RobotChallenge {
                 scent[0] = location[0];
                 scent[1] = location[1];
                 scentN = "LOST";
-                System.out.println("dead : " + scent[0] +", " + scent[1]);
             }
         }
 
